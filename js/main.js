d4838e93d4dd7931c5d03a8b127380fcf5524262
Chibu <atuchibueze@gmail.com>
2022-08-16 17:38:05 -0400

    implement delete bookmark function

--- js/main.js
+++ js/main.js
@@ -3,8 +3,8 @@ document.getElementById("myForm").addEventListener("submit", saveBookmark);
 
 function saveBookmark(e) {
   // get form values
-  const siteName = document.getElementById("siteName").value;
-  const siteUrl = document.getElementById("siteUrl").value;
+  let siteName = document.getElementById("siteName").value;
+  let siteUrl = document.getElementById("siteUrl").value;
 
   const bookmark = {
     name: siteName,
@@ -34,5 +34,54 @@ function saveBookmark(e) {
     localStorage.setItem("bookmarks", JSON.stringify(bookmarks));
   }
 
+  fetchBookmarks();
+
+  // set input values to empty
+  document.getElementById("siteName").value = "";
+  document.getElementById("siteUrl").value = "";
+
   e.preventDefault();
+}
+
+// delete bookmark
+function deleteBookmark(url) {
+  // Fetch bookmarks from local storage
+  let bookmarks = JSON.parse(localStorage.getItem("bookmarks"));
+
+  let newBookmarks = bookmarks.filter((bookmark) => {
+    if (bookmark.url !== url) {
+      return bookmark;
+    }
+  });
+
+  console.log(newBookmarks);
+
+  localStorage.setItem("bookmarks", JSON.stringify(newBookmarks));
+
+  fetchBookmarks();
+}
+
+function fetchBookmarks() {
+  // Fetch bookmarks from local storage
+  let bookmarks = JSON.parse(localStorage.getItem("bookmarks"));
+
+  // get output id
+  const bookmarksResults = document.getElementById("bookmarksResults");
+
+  // build output
+  let html = bookmarks
+    .map(
+      (bookmark) => `
+    <div class='well'>
+    <h3>
+    ${bookmark.name} 
+    <a class='btn btn-default' target='_blank' href='${bookmark.url}'>Visit</a> 
+    <a onclick='deleteBookmark("${bookmark.url}")' class='btn btn-danger' href='#'>Delete</a> 
+    </h3>
+    </div>
+    `
+    )
+    .join("");
+
+  bookmarksResults.innerHTML = html;
 }
